a034a40e8cb70dbb4abd4f5a979b0b1e84e952e5
Charlie Brassington <realcharliebrassington@gmail.com>
2024-12-10 05:26:17 +0000

    added impl activation funcs

---
+++ multilayer_perceptron/activation_functions/sigmoid_activation.py
@@ -0,0 +1,7 @@
+from multilayer_perceptron.activation_functions.base_exponent_activation import BaseExponentActivation
+
+
+class SigmoidActivation(BaseExponentActivation):
+
+    def transform(self, number: int) -> float:
+        return 1 / (1 + self.exponent_helper.calculate_exponent(-number))
